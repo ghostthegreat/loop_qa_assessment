ecd2d6b75e57afbbac3f99fdb5124914f74054ab
ghostthegreat <ghostthegreat313@gmail.com>
2024-11-26 20:00:48 -0500

    Delete test_base_page.py

--- test_base_page.py
+++
@@ -1,6 +0,0 @@
-from playwright.async_api import Page
-class BasePage:
-    def __init__(self, page: Page):
-        self.page = page
-    def find_element(self, locator):
-        return self.page.locator(locator)
